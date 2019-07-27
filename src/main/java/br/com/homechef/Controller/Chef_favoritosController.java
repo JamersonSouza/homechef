9c6d0a21d09114f01f0b9fc518dd8d54502ebfcf
Natalia Regis <naty.lima49@gmail.com>
2019-07-27 12:36:13 -0300

    lista de chef favoritos funcionando

--- src/main/java/br/com/homechef/Controller/Chef_favoritosController.java
+++ src/main/java/br/com/homechef/Controller/Chef_favoritosController.java
@@ -1,13 +1,18 @@
 package br.com.homechef.Controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 import br.com.homechef.model.Chef_favoritos;
+import br.com.homechef.service.Chef_favoritosService;
 
 @Controller
 public class Chef_favoritosController {
+	
+	@Autowired
+	private Chef_favoritosService chef_favoritosService;
 	
 	//metodo apenas para exibir a view a pagina que possue o botao favoritar
 		@GetMapping("/chef_encontrarChef")
@@ -21,7 +26,7 @@ public class Chef_favoritosController {
 		@GetMapping("/lista_chefes_favoritos")
 			public ModelAndView listaChefFavoritos() {
 				ModelAndView mv = new ModelAndView("lista_chefes_favoritos");
-			//mv.addObject("listaChefFav", this.chef_favoritosService.findAll());
+			mv.addObject("listaChefFav", this.chef_favoritosService.findAll());
 				return mv;
 			}
 
