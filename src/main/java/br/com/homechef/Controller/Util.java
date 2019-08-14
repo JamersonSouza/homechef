20582698594222833c8b2483df2981fb8dc13d15
Jamerson Souza <jamersonsouza@outlook.com>
2019-08-14 20:06:50 -0300

    Primeiros Passos para o Pagamento

--- src/main/java/br/com/homechef/Controller/Util.java
+++ src/main/java/br/com/homechef/Controller/Util.java
@@ -3,8 +3,22 @@ package br.com.homechef.Controller;
 import java.io.BufferedOutputStream;
 import java.io.File;
 import java.io.FileOutputStream;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 import org.springframework.web.multipart.MultipartFile;
+
+import com.paypal.api.payments.Amount;
+import com.paypal.api.payments.Links;
+import com.paypal.api.payments.Payer;
+import com.paypal.api.payments.Payment;
+import com.paypal.api.payments.RedirectUrls;
+import com.paypal.api.payments.Transaction;
+import com.paypal.base.rest.APIContext;
+import com.paypal.base.rest.PayPalRESTException;
+
 
 public class Util {
 	
@@ -35,6 +49,99 @@ public class Util {
 		}
 		return sucessoUpload;
 		}
+	
+	public static String createPayment(String sum){
+
+		 String redirectUrl = "";
+
+		String clientId = "AWcK0IFdoSBpTwmzEkojt_zU918mrbOT7nP5-SfwCituIJDbnuF3QzLth52HxhJ7hJ_36f1OTCfV2KFi";
+
+		String clientSecret = "EDlPgmlXlD0S-VbkQepqPfmhHyyniuIFBdiG2HXnN883Wx6rBGEl7fDCQPSUhbVq0-Ep0uhTLjUajY7x";
+
+        Map<String, Object> response = new HashMap<String, Object>();
+
+        Amount amount = new Amount();
+
+        amount.setCurrency("USD");
+
+        amount.setTotal(sum);
+
+        Transaction transaction = new Transaction();
+
+        transaction.setAmount(amount);
+
+        List<Transaction> transactions = new ArrayList<Transaction>();
+
+        transactions.add(transaction);
+
+
+
+        Payer payer = new Payer();
+
+        payer.setPaymentMethod("paypal");
+
+
+
+        Payment payment = new Payment();
+
+        payment.setIntent("sale");
+
+        payment.setPayer(payer);
+
+        payment.setTransactions(transactions);
+
+
+
+        RedirectUrls redirectUrls = new RedirectUrls();
+
+        redirectUrls.setCancelUrl("http://localhost:8080/cancel");
+
+        redirectUrls.setReturnUrl("http://localhost:8080");
+
+        payment.setRedirectUrls(redirectUrls);
+
+        Payment createdPayment;
+
+        try {
+
+
+            APIContext context = new APIContext(clientId, clientSecret, "sandbox");
+
+            createdPayment = payment.create(context);
+
+            if(createdPayment!=null){
+
+                List<Links> links = createdPayment.getLinks();
+
+                for (Links link:links) {
+
+                    if(link.getRel().equals("approval_url")){
+
+                        redirectUrl = link.getHref();
+
+                        System.out.println(redirectUrl);
+
+                        break;
+
+                    }
+
+                }
+
+                response.put("status", "success");
+
+                response.put("redirect_url", redirectUrl);
+
+            }
+
+        } catch (PayPalRESTException e) {
+
+            System.out.println("Error happened during payment creation!");
+
+        }
+
+        return redirectUrl;
+
+    }
 
 
 }
